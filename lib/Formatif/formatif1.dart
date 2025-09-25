efba858b15a7f55a40fd483a3825ec1e34e2b951
Memphissall <memphissall580.com>
2025-09-25 22:09:06 +0700

    Colum atau card ke 2 eror

--- lib/Formatif/formatif1.dart
+++ lib/Formatif/formatif1.dart
@@ -65,18 +65,67 @@ Container(
   height: 20,
   decoration: BoxDecoration(color: Colors.white,shape: BoxShape.circle),
   child: Icon(Icons.play_arrow,color:Colors.purple,size: 10),
-
-)
-
+),
                 ],
               ),
             ],
           ),
         ),
+
+
+        SizedBox(height: 20),
+        Container(
+          width: 300,
+          height: 120,
+          padding: EdgeInsets.all(16),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("gambar/run.png"),
+                  fit: BoxFit.contain,
+              ),
+            ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "You Are Doing Greate",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    "Keep It Up",
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 14,
+                    ),
+                  ),
+                  Text(
+                    "Stick To Your Plan",
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              ),
+          ],
+        ),
       ),
-
-
-
+      
     );
   }
 }
